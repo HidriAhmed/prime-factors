cef414d64576f75f2f8d146da451e6fe9eebb8e9
Ahmed Hidri <hidri.ahmd@gmail.com>
2014-04-02 13:43:14 +0200

    Code refactoring for the test

--- src/test/java/com/olbati/primefactors/PrimeFactorsTest.java
+++ src/test/java/com/olbati/primefactors/PrimeFactorsTest.java
@@ -4,27 +4,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class PrimeFactorsTest {
 
+	PrimeFactors primeFactors;
+
+	@Before
+	public void init() {
+		primeFactors = new PrimeFactors();
+	}
+
 	@Test
 	public void given1001_shouldGenerate7x11x13() {
-		PrimeFactors primeFactors = new PrimeFactors();
 		List<Integer> result = primeFactors.generate(1001);
 		assertThat(result).containsExactly(7, 11, 13);
 	}
 
 	@Test
 	public void given20_shouldGenerate2x2x5() {
-		PrimeFactors primeFactors = new PrimeFactors();
 		List<Integer> result = primeFactors.generate(20);
 		assertThat(result).containsExactly(2, 2, 5);
 	}
 
 	@Test
 	public void given7_shouldGenerate7() {
-		PrimeFactors primeFactors = new PrimeFactors();
 		List<Integer> result = primeFactors.generate(7);
 		assertThat(result).containsExactly(7);
 	}
@@ -38,14 +43,12 @@ public class PrimeFactorsTest {
 
 	@Test
 	public void givenPrimeNumber_shouldRTeturnTrue() {
-		PrimeFactors primeFactors = new PrimeFactors();
 		boolean result = primeFactors.isPrimeNumber(5);
 		assertThat(result).isTrue();
 	}
 
 	@Test
 	public void givenNotPrimeNumber_shouldRTeturnFalse() {
-		PrimeFactors primeFactors = new PrimeFactors();
 		boolean result = primeFactors.isPrimeNumber(4);
 		assertThat(result).isFalse();
 	}
